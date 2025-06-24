0cd561cd7bc5bbd7e967009da89a1d1157d09390
Choi <cjy1749@naver.com>
2025-06-24 16:50:42 +0900

    .md file modified commit~

--- src/chapter02/HelloJava.java
+++ src/chapter02/HelloJava.java
@@ -2,9 +2,9 @@ package chapter02;
 
 public class HelloJava {
 
-	//main 메소드 - 실행을 위한 필수 메소드	
 	public static void main(String[] args) {
-		System.out.println("Welcome to JavaWorld!!!");	
+		System.out.println("Welcome to JAVA Worlds!!!");
+		System.out.println("Hello~");
 	}
 
 }
