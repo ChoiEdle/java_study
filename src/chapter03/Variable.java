d5fb198e86e60b8e469f63c8732f25677b5af2d2
Choi <cjy1749@naver.com>
2025-06-25 12:49:06 +0900

    .md file modified commit~

--- src/chapter03/Variable.java
+++ src/chapter03/Variable.java
@@ -1,4 +1,5 @@
 package chapter03;
+//import java.lang.String;
 
 public class Variable {
 	public static void main(String[] args) {
@@ -7,7 +8,24 @@ public class Variable {
 		double aged = 10.0;
 		boolean flag = true;	//true(1), false(0)
 		char name = '홍';
+		char name2 = '길';
+		char name3 = '동';
+		String sname = "김길동";					//기본형(Stack에 저장됨)
+		
+		// 참조 자료 변수 정의
+		String sname2 = new String("이길동");		//참조형(Heap에 저장됨)		
+		// ** String 클래스는 기본형과 참조형으로 모두 사용 가능!!
+				
+		//출력
 		System.out.println(age);
+		System.out.println(aged);
+		System.out.println(flag);
+		System.out.print(name);
+		System.out.print(name2);
+		System.out.println(name3);
+		System.out.println(sname);
+		System.out.println(sname2);
+
 	}
 
 }
