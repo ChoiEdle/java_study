0a1ec4288eb30f5211f970d2e442794e840e865e
Choi <cjy1749@naver.com>
2025-06-26 18:27:15 +0900

    chapter5 file commit~

--- src/chapter05/LunchOrderTestVer1_Review.java
+++ src/chapter05/LunchOrderTestVer1_Review.java
@@ -14,6 +14,24 @@ public class LunchOrderTestVer1_Review {
 		System.out.println("\t9. 나가기");		
 		System.out.println("*******************************************");
 		//1. 메뉴 선택
+		int menuNo = -1;
+		String menuName = " ";
+		int menuPrice = 0;
+		
+		Scanner scan = new Scanner(System.in);
+		if(scan.hasNextInt()) {
+			switch(menuNo = scan.nextInt()){
+			case 1 : menuName = "햄버거(🍔)"; menuPrice = 100; break;
+			case 2 : menuName = "피자(🍕)"; menuPrice = 200; break;
+			case 3 : menuName = "라멘(🍜)"; menuPrice = 300; break;
+			case 4 : menuName = "샐러드(🥗)"; menuPrice = 400; break;
+			case 9 : System.out.println("나가기"); break;
+			default : System.out.println("메뉴를 준비중 입니다.");
+				
+			}
+		}else {
+			System.out.println("잘못 입력 하셨습니다.");
+		}
 		//2. 주문 결제
 		//3. 주문 내역 출력 : 주문한 메뉴는 (햄버거), 결제금액(), 잔돈() 입니다.
 		}
