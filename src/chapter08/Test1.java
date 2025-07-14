1061bb8b72e57b2b5cb46a55cccb50801ca4273a
choi <cjy1749@naver.com>
2025-07-14 17:40:11 +0900

    07-14 commit

--- src/chapter08/Test1.java
+++ src/chapter08/Test1.java
@@ -3,7 +3,7 @@ package chapter08;
 abstract public class Test1 {
 	String name;
 	public Test1() {
-		
+		System.out.println("Test1 생성");
 	}
 	
 	abstract public void plintInfo();
--- src/chapter08/Test2.java
+++ src/chapter08/Test2.java
@@ -3,6 +3,7 @@ package chapter08;
 public class Test2 extends Test1 {
 	
 	public Test2(String name){
+		System.out.println("Test2 생성");
 		super.name = name;
 	}
 	
