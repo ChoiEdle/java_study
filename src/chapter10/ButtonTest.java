e88579d38e8cc1de5e988f3d4b9597743d514c77
choi <cjy1749@naver.com>
2025-07-15 15:24:24 +0900

    07-15 commit

---
+++ src/chapter10/ButtonTest.java
@@ -0,0 +1,13 @@
+package chapter10;
+
+public class ButtonTest {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/chapter10/Outer.java
@@ -0,0 +1,15 @@
+package chapter10;
+
+public class Outer {
+	//Field
+	String name = "홍길동";
+	
+	//Constructor
+	
+	//Method
+	
+	//Inner class
+	public static class Inner {
+		static String name = "이순신";
+	}
+}
