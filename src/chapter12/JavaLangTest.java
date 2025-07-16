0b204e4f26301e46bc9f81431e60c27914554e7d
choi <cjy1749@naver.com>
2025-07-16 12:48:58 +0900

    07-16 commit

---
+++ src/chapter12/JavaLangTest.java
@@ -0,0 +1,21 @@
+package chapter12;
+
+public class JavaLangTest {
+
+	public static void main(String[] args) {
+		int num1 = 100;
+		Integer num2 = new Integer(num1);		//Deprecated
+		Integer num3 = Integer.valueOf(num1);
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		
+		
+		
+		
+		
+		
+	}
+
+}
